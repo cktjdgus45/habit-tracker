a10d2b6babb97527e66fcd80da55f18d9f1d82a4
pooh <cktjdgus45@naver.com>
2021-12-22 18:06:25 +0900

    handle state and modify function in Habits Component not Habit

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0,
-    }
+
     handleIncreament = () => {
-        //state 오브젝트 안의 count 변경 후 state를 업데이트.
-        this.setState({ count: this.state.count + 1 });
-    }
-    handledecreament = () => {
-        //state 오브젝트 안의 count 변경 후 state를 업데이트.
-        const count = this.state.count - 1;
-        this.setState({ count: count < 0 ? 0 : count });
-    }
+        this.props.onIncreament(this.props.habit);
+    };
+    handleDecreament = () => {
+        this.props.onDecreament(this.props.habit);
+    };
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
     render() {
         const { name, count } = this.props.habit;
         return (
@@ -22,10 +20,10 @@ class Habit extends Component {
                 <button onClick={this.handleIncreament} className="habit-button habit-increase">
                     <i className="fas fa-plus-square"></i>
                 </button>
-                <button onClick={this.handledecreament} className="habit-button habit-decrease">
+                <button onClick={this.handleDecreament} className="habit-button habit-decrease">
                     <i className="fas fa-minus-square "></i>
                 </button>
-                <button className="habit-button habit-delete">
+                <button onClick={this.handleDelete} className="habit-button habit-delete">
                     <i className="fas fa-trash"></i>
                 </button>
             </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -9,11 +9,25 @@ class Habits extends Component {
             { id: 3, name: 'Coding', count: 0 },
         ]
     }
+    handleIncreament = (habit) => {
+        console.log(`handle Increament ${habit}`);
+    };
+    handleDecreament = (habit) => {
+        console.log(`handle Decreament ${habit}`);
+    };
+    handleDelete = (habit) => {
+        console.log(`handle delete ${habit}`);
+    };
     render() {
         return (
             <ul>
                 {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit} />
+                    <Habit
+                        key={habit.id}
+                        habit={habit}
+                        onIncreament={this.handleIncreament}
+                        onDecreament={this.handleDecreament}
+                        onDelete={this.handleDelete} />
                 )
                 )}
             </ul>
