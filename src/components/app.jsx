f1c715b27bbdbbf8e147907b946e5e3e73a62995
pooh <cktjdgus45@naver.com>
2021-12-22 21:06:12 +0900

    add Nav Component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,45 @@
 import '../app.css';
 import Habits from './habits';
 
-function App() {
-  return (
-    <Habits></Habits>
-  );
+import React, { Component } from 'react';
+import Navbar from './navbar';
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ]
+  }
+  handleIncreament = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+  handleDecreament = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits: habits })
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits: habits })
+  };
+  render() {
+    return (
+      <>
+        <Navbar totalCount={this.state.habits.filter(item => item.count !== 0).length} />
+        <Habits habits={this.state.habits}
+          onIncreament={this.handleIncreament}
+          onDecreament={this.handleDecreament}
+          onDelete={this.handleDelete} />
+      </>
+    );
+  }
 }
 
 export default App;
