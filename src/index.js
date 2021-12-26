e1c7a8165aef774ec53dd06a1092f439441fc3bd
pooh <cktjdgus45@naver.com>
2021-12-26 20:14:23 +0900

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ReactHook from './components/reactHook.jsx';
+import App from './components/app.jsx';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactHook />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
