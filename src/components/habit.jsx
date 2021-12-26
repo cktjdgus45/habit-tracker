a3b3695d8fe79c04122dfda9ef7f09f2e576baf7
pooh <cktjdgus45@naver.com>
2021-12-26 17:29:13 +0900

    study about Life Cycle

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,7 +1,12 @@
 import React, { PureComponent } from 'react';
 
 class Habit extends PureComponent {
-
+    componentDidMount() {
+        console.log(`habit:${this.props.habit.name} mounted`)
+    }
+    componentWillUnmount() {
+        console.log(`habit:${this.props.habit.name} will unmount`)
+    }
     handleIncreament = () => {
         this.props.onIncreament(this.props.habit);
     };
